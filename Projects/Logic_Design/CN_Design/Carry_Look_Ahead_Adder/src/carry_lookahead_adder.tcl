b7366de89ab2d3328b5714f36b238942c45cf8a6
Xilinx University Program <xupgit@gmail.com>
2015-06-03 12:23:28 -0700

    Updated tcl

--- Projects/Logic_Design/CN_Design/Carry_Look_Ahead_Adder/src/carry_lookahead_adder.tcl
+++ Projects/Logic_Design/CN_Design/Carry_Look_Ahead_Adder/src/carry_lookahead_adder.tcl
@@ -32,7 +32,7 @@ set constraints_file carry_lookahead_adder_basys3_pins.xdc
 set testbench carry_lookahead_adder_tb.v
 # Create project for Basys 3
 create_project -force $project_name ./$project_name -part xc7a35tcpg236-1
-set_property board_part xilinx.com:basys3:part0:1.1 [current_project]
+set_property board_part digilentinc.com:basys3:part0:1.1 [current_project]
 set_property target_language verilog [current_project]
 set_property simulator_language Verilog [current_project]
 set_property ip_repo_paths  $basys3_github [current_project]
